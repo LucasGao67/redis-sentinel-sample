b8788a5b66eae56eeb3df4d042ea3aba142dda48
gaoke <ke.gao@horizon.ai>
2018-11-13 15:32:08 +0800

    update

--- run.sh
+++ run.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
+
+#===============
+#redis相关
+#===============
+# redis密码
 export REDIS_PWD=123456
+# redis ip
 export MASTER_IP="127.0.0.1"
+# redis master 端口
 export MASTER_PORT="6379"
+
+
+
+#===============
+#sentinel相关
+#===============
 export SENTINEL_IP="127.0.0.1"
 export SENTINEL_PORT_1="26381"
 export SENTINEL_PORT_2="26382"
 export SENTINEL_PORT_3="26383"
 export SENTINEL_MASTER_NAME="myredis"
-export SENTINEL_MASTER_IP="127.0.0.1"
 
 docker-compose up $@
