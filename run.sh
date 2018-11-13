62f0449ac471f02ff5c2a9736eea68832f282943
gaoke <ke.gao@horizon.ai>
2018-11-13 14:51:34 +0800

    修改环境变量

--- run.sh
+++ run.sh
@@ -6,5 +6,6 @@ export SENTINEL_IP="127.0.0.1"
 export SENTINEL_PORT_1="26381"
 export SENTINEL_PORT_2="26382"
 export SENTINEL_PORT_3="26383"
+export SENTINEL_MASTER_NAME="myredis"
 
 docker-compose up $@
